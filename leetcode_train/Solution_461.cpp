56509ae508d82368ef225d5a4718d0f7f3422432
li_jiax <li_jiax@DESKTOP-HPQ1AKC>
2019-10-22 15:47:43 +0800

    2019_10_22

---
+++ leetcode_train/Solution_461.cpp
@@ -0,0 +1,20 @@
+//The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+//
+//Given two integers x and y, calculate the Hamming distance.
+
+//两个数进行异或，然后计算该数字二进制数中1的个数
+
+
+class Solution {
+public:
+	int hammingDistance(int x, int y) {
+		int temp = x^y;
+		int rs = 0;
+		while (temp != 0) {
+			rs++;
+			temp = temp&(temp - 1);
+		}
+		return rs;
+
+	}
+};
